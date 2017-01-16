30a03ddd1ccd9a5b26dd1076dbfd86ba63f885de
Stefan Penner <stefan.penner@gmail.com>
2017-01-15 16:27:12 -0800

    more cleanup

--- conf.d/node.fish
+++ conf.d/node.fish
@@ -1,16 +1,12 @@
 set -g FISH_NODE_ROOT $HOME/.config/node
 set -g FISH_NODE_REMOTE "https://nodejs.org/download/release"
 
-function _get_node_current_platform
-  set -l os (uname -s)
-  set -l arch (uname -m)
-  set -l x (echo $os | tr '[A-Z]' '[a-z]')
-  set -l y (echo $arch | sed s/86_//)
+set -l os (uname -s)
+set -l arch (uname -m)
+set -l x (echo $os | tr '[A-Z]' '[a-z]')
+set -l y (echo $arch | sed s/86_//)
 
-  echo "$x-$y"
-end
-
-set -g _node_current_platform (_get_node_current_platform)
+set -g _node_current_platform "$x-$y"
 
 function __echo-success -a message
   set_color green;
@@ -31,13 +27,8 @@ function node-setup
 end
 
 node-setup
-# clean_node_version v6.7.0
-# node-isntall v6.7.0
-# node-isntall v4.7.0
-# node-ls
-# node-ls-remote
 
-if test -f $HOME/.config/node/default/bind
+if test -f $HOME/.config/node/default/bin
   set -gx PATH $HOME/.config/node/default/bin $PATH
 end
 
